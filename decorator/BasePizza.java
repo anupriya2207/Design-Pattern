436148b0642e7cfa3e3c191f1d969238481d749f
anupriya2207 <97846264+anupriya2207@users.noreply.github.com>
2024-10-30 21:57:27 +0530

    Update BasePizza.java

--- decorator/BasePizza.java
+++ decorator/BasePizza.java
@@ -3,3 +3,29 @@ package DecoratorDesign;
 public abstract class BasePizza {
 	public abstract int cost(); 
 }
+
+
+So when I joined, I was part of Pathfinders team, so I had learnt about FDX and TPCS both and once I got hands on,
+
+  I Implemented handling of TANs as part of consent modernization
+  Performance testing for VDBS service, after JAVA version upgarde
+  Performane testing for consent status job after migration
+  Then I mostly worked on Daily job to retry failed VAN updates, preparation of test cases, QA validations, Performnace testing with Dual Write, Reporting
+  Also I worked on Photon upgrade for VDBS service
+  
+Now I'm part of Trailblazers team, so here i have worked on following items:
+
+	I did photon upgrade for consent-db-migration service
+	Production deployment after poton upgrade for PDR service and consent-db-migration service
+	Got KT on app onboarding services, which was really helpful to understand the entire flow
+	Did PVT testing for CAOE service and was validated in perf
+	Deployment for CAOE for new pools addition, and have done prod deployment multiple times and got proper hands on
+	Have worked on consent-db-migration which is our liquibase repo for craeting PBB tables, also loading data to those tables through liquibase, as part of this, i also wrote a script which will take excel files as input and converts it to sql queries, so that next time data can be loaded with minimal efforts.
+	Integrated AEM new endpoints with our CCAR service for target state
+	Did PDR migration and then ran PDR RECON jobs in perf environment and analyzed teh mistamches
+	Will do PDR recon in PROD as well
+	Also got a chance to shadow Production support, and got KT for that, so that i can next time handle it on my own
+	
+	Took AWS developer assocoiate tarining and will take the exam 
+  
+  

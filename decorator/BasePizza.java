aa1e4eedf78ee6226a49ecd310d19f29971c3827
anupriya2207 <97846264+anupriya2207@users.noreply.github.com>
2024-11-08 15:46:24 +0530

    Update BasePizza.java

--- decorator/BasePizza.java
+++ decorator/BasePizza.java
@@ -4,5 +4,4 @@ public abstract class BasePizza {
 	public abstract int cost(); 
 }
 
-
-=IF(LEFT(B2,1)="1", SUMIFS(C:C, A:A, A2, B:B, LEFT(B2,1)&"*") = 1, TRUE)
+=IF(LEFT(B2,1)="1", SUMIFS(C:C, A:A, A2, B:B, "1*") = 1, TRUE)
